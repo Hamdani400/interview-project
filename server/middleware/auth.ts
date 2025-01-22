c1cf8de3ac8fdcfd46efe61f50d7ca3f9c3e3b21
dani <dinulhaqhamdani41@gmail.com>
2025-01-23 00:50:08 +0700

    create server middleware

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -5,9 +5,7 @@ import { sendRedirect } from 'h3'
 export default defineEventHandler(async (event) => {
     try {
         const user_id = getCookie(event, 'user_id')
-        const response = await axios.get(`https://jsonplaceholder.typicode.com/users/${user_id}`);
-        const isAuthenticated = !!response.data.id
-        console.log(isAuthenticated)
+        await axios.get(`https://jsonplaceholder.typicode.com/users/${user_id}`);
     } catch (error) {
         const currentPath = event.req.url || ''
         if (['/dashboard', '/dashboard/vip', '/dashboard/not-vip'].includes(currentPath)) {
