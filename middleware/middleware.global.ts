fc6cd422812930a44cf1149f8b0c8a9456df2627
dani <dinulhaqhamdani41@gmail.com>
2025-01-23 09:39:41 +0700

    fix middleware client

--- middleware/middleware.global.ts
+++ middleware/middleware.global.ts
@@ -5,15 +5,15 @@ export default defineNuxtRouteMiddleware(async (to, from) => {
     const user_id = useCookie('user_id')
     const user_data = useUserDataStore()
     const { setUserData } = user_data
+    const { getUserData } = user_data
 
-    if (user_id.value) {
+    if (user_id.value && Object.keys(getUserData).length === 0) {
         const response = await axios.get(
             `http://localhost:3000/api/auth/${user_id.value}`
         );
         setUserData(response.data.data)
     }
 
-    const { getUserData } = user_data
 
     if (user_id.value && to.path === '/') {
         return navigateTo('/dashboard')
